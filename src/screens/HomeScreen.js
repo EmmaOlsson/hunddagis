74ffd70c56b80b00490b1d43253e76b415a7b0b8
elinkronqvist <elin.kronqvist@utb.ecutbildning.se>
2021-08-31 16:04:24 +0200

    main img + info

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,17 +7,27 @@ import happyDogsImg from '../../src/assets/graphics/happy-dogs.jpg';
 const HomeScreen = () => {
   return (
     <section>
-      <div>
-      <img className={Styles.mainImg} src={happyDogsImg} alt="picture-of-happy-dogs" ></img>
-      <article className={Styles.welcomeText} >
+      
+
+      <main className={Styles.mainContainer} >
+        
+      <img className={Styles.mainImg} src={happyDogsImg} alt="happy-dogs" ></img>
+
+      <div className={Styles.mainText} >
+      <article className={Styles.infoBox} >
         <h3>Hundkojans hunddagis</h3>
         <p>Våra öppettider</p>
         <p>Inlämning: 07:00 - 09:00</p>
         <p>Hämtning: 14:00 - 17:30</p>
       </article>
-      <h5>Intresserad av en dagisplats?</h5>
-      <button className={Styles.applicationButton} >Skicka ansökan</button>
+      <article className={Styles.applicationBox} >
+        <h5>Intresserad av en dagisplats?</h5>
+        <button className={Styles.applicationButton} >Skicka ansökan</button>
+      </article>
       </div>
+      </main>
+
+
     </section>
   )
 };
