aeb709dcf69fbc78d1251f9b8ffc5a04b0461a83
elinkronqvist <elin.kronqvist@utb.ecutbildning.se>
2021-08-31 11:56:07 +0200

    graphics

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Styles from '../css/homescreen.module.css';
+
+import happyDogsImg from '../../src/assets/graphics/happy-dogs.jpg';
+
 const HomeScreen = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <img className={Styles.mainImg} src={happyDogsImg} alt="picture-of-happy-dogs" ></img>
+    </section>
+  )
 };
 
 export default HomeScreen;
