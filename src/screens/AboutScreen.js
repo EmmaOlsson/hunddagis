cb54380ac2a8291c3da1727ea13a7b7db78acefa
Emma Olsson <emma.olsson22@learnet.se>
2021-08-31 13:15:29 +0200

    added logo

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Styles from '../css/about.module.css'
 
+import logoDog from '../assets/graphics/Hund-logo-color.png'
+
 const AboutScreen = () => {
   return <section>
-        <div>
-          <p className={Styles.text}>hej</p>
-        </div>        
-
-        </section>;
+    <main>
+    <article className={Styles.aboutText}>
+      <div>
+        <img className={Styles.logoDog} src={logoDog} alt='logo-dog'></img>
+      </div>
+      <div>
+        <img className={Styles.logoDog} src={logoDog} alt='logo-dog'></img>
+      </div>
+      <div>
+      <img className={Styles.logoDog} src={logoDog} alt='logo-dog'></img>
+      </div>
+      </article>
+    </main>
+  </section>;
 };
 
 export default AboutScreen;
