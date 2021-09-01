f303db7853f2b8f7523fe1924d08fc872db77903
Nina Jivetorp <ninajivetorp@gmail.com>
2021-09-01 11:34:55 +0200

    added state w dummy-array of questions

--- src/screens/FaqScreen.js
+++ src/screens/FaqScreen.js
@@ -1,6 +1,39 @@
 import React from 'react';
 
 const FaqScreen = () => {
+  const [questions, setQuestions] = useState([
+    {
+      question: 'question1',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+    {
+      question: 'question2',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+    {
+      question: 'question3',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+    {
+      question: 'question4',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+    {
+      question: 'question5',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+    {
+      question: 'question6',
+      answer:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+    },
+  ]);
+
   return <div>FAQ</div>;
 };
 
