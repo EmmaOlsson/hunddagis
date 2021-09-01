59ab5b5c858775fe11fbb9de1de1cc0b94394ebd
elinkronqvist <elin.kronqvist@utb.ecutbildning.se>
2021-09-01 15:43:45 +0200

    start

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import Styles from '../css/footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section>
+        <footer className={Styles.mainFooter} >
+            <div className={Styles.footerTop} >
+            <nav className={Styles.navFooter} >
+              <ul className={Styles.navFooter} >
+                <li className={Styles.navFooterItem} >Blogg</li>
+                <li className={Styles.navFooterItem} >Om oss</li>
+                <li className={Styles.navFooterItem} >Vanliga frågor</li>
+                <li className={Styles.navFooterItem} >Kontakt</li>
+              </ul>
+            </nav>
+            </div>
+
+            <div className={Styles.footerBottom} >
+              <p>&copy; Hundkojan AB 2021</p>
+            </div>
+
+        </footer>
+    </section>
+  )
+  
+
 };
 
 export default Footer;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,31 +1,46 @@
 import React from 'react';
 
 import Styles from '../css/homescreen.module.css';
-
+import { useHistory } from 'react-router-dom';
 import happyDogsImg from '../../src/assets/graphics/happy-dogs.jpg';
 
 const HomeScreen = () => {
+
+  const history = useHistory();
+
+  function handleClick() {
+    history.push("/contact");
+  }
+
   return (
-    <section>
+    <section className={Styles.mainSection}>
       
 
       <main className={Styles.mainContainer} >
-        
+
       <img className={Styles.mainImg} src={happyDogsImg} alt="happy-dogs" ></img>
 
-      <div className={Styles.mainText} >
-      <article className={Styles.infoBox} >
-        <h3>Hundkojans hunddagis</h3>
-        <p>Våra öppettider</p>
-        <p>Inlämning: 07:00 - 09:00</p>
-        <p>Hämtning: 14:00 - 17:30</p>
-      </article>
-      <article className={Styles.applicationBox} >
-        <h5>Intresserad av en dagisplats?</h5>
-        <button className={Styles.applicationButton} >Skicka ansökan</button>
-      </article>
-      </div>
       </main>
+      <div className={Styles.mainText} >
+        <article className={Styles.infoBox} >
+          <h3>Hundkojans hunddagis</h3>
+          <p>Våra öppettider</p>
+          <p>Inlämning: 07:00 - 09:00</p>
+          <p>Hämtning: 14:00 - 17:30</p>
+        </article>
+
+
+        <article className={Styles.applicationBox} >
+          <div className={Styles.applicationBoxCircle} ></div>
+
+          <div className={Styles.applicationBoxContent} >
+            <h5>Intresserad av en dagisplats?</h5>
+          <button className={Styles.applicationButton} onClick={handleClick} >Skicka ansökan</button>
+            </div>
+
+        </article>
+
+        </div>
 
 
     </section>
