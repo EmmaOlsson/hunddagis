77204a30a7cf7110128a5f991c593bdc4f1b0687
Nina Jivetorp <ninajivetorp@gmail.com>
2021-09-02 11:37:27 +0200

    changed logo and added flags for language

---
+++ src/assets/graphics/faqData.js
@@ -0,0 +1,34 @@
+const faqData = [
+  {
+    question: 'Hur många hundar tar ni emot samtidigt?',
+    answer:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+  {
+    question: 'Hur ser en dag ut hos er?',
+    answer:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+  {
+    question: 'Hur lång tid tar det att få en dagisplats?',
+    answer:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+  {
+    question: 'Tar ni emot alla raser?',
+    answer:
+      'Lorem, ipsum dolor sit amet conectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+  {
+    question: 'Kan man ha hundar över natten hos er?',
+    answer:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+  {
+    question: 'Tar ni emot okastrerade hundar?',
+    answer:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis earum corrupti officia magni, quisquam cumque dolorem praesentium',
+  },
+];
+
+export default faqData;
