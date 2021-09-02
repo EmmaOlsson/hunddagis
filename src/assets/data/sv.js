3e85a1148ce22be454bc55a52c3aae55ecf26744
Nina Jivetorp <ninajivetorp@gmail.com>
2021-09-02 15:59:38 +0200

    updated faq-translation to array

--- src/assets/data/sv.js
+++ src/assets/data/sv.js
@@ -1,101 +1,106 @@
 export default {
-    navbar: {
-        about: "Om oss",
-        blog: "Blogg",
-        questions: "Vanliga Frågor",
-        contact: "Kontakt"
+  navbar: {
+    about: 'Om oss',
+    blog: 'Blogg',
+    questions: 'Vanliga Frågor',
+    contact: 'Kontakt',
+  },
+
+  startPage: {
+    title: 'Välkommen till Hundkojans hunddagis',
+  },
+
+  contact: {
+    title: 'Kontakta oss',
+
+    contactDetails: {
+      title: 'Kontaktuppgifter',
+      name: 'Namn:',
+      address: 'Adress:',
+      phone: 'Telefon:',
+      Email: 'E-post:',
+    },
+  },
+
+  faq: {
+    title: 'Vanliga Frågor',
+    subHeading:
+      'Självklart har du som ägare av en fyrbent vän många frågor. Här har vi samlat de vanligaste frågorna med svar. Skulle svaret på din fråga inte finnas är du välkommen att kontakta oss här!',
+
+    questions: [
+      {
+        title: 'Hur många hundar tar ni emot samtidigt?',
+        answer: '',
+      },
+      {
+        title: 'Hur ser en dag ut hos er?',
+        answer: '',
+      },
+      {
+        title: 'Hur lång tid tar det att få en dagisplats?',
+        answer: '',
+      },
+      {
+        title: 'Tar ni emot alla raser?',
+        answer: '',
+      },
+      {
+        title: 'Kan man ha hundar över natten hos er?',
+        answer: '',
+      },
+      {
+        title: 'Tar ni emot okastrerade hundar?',
+        answer: '',
+      },
+    ],
+  },
+
+  about: {
+    title: '- Vi tar hand om din bästa vän',
+
+    firstParagraph:
+      'Hundkojan hittar du på en stor familjegård bara 5 minuter från Helsingborgs centrum. Gården har stora inhängnade utomhusytor där din fyrbenta vän kan springa fritt med andra fyrbenta kompisar. Inomhusdelen renoverades under 2020 och där finns både sociala och indivduella utrymmen, allt för att din hund ska känna sig trygg.',
+
+    secondParagraph:
+      'Hundkojan ser till att din vovve både får lugn och ro, aktivering, stimulans och härligt umgänge med andra hundar – så att du kan göra det du behöver göra!',
+
+    thirdParagraph:
+      'Du väljer själv vilken tjänst som passar dig och din hund bäst!',
+
+    fourthParagraph:
+      'Smulan och Bosse Hundsson grundade 1998 Hundkojan AB och har sedan dess samlat på sig väldigt många fyrbenta vänner runtom i Skåne. Verksamheten är godkänd av Länsstyrelsen.',
+
+    ourServices: {
+      title: 'Våra tjänster',
+
+      fullTime: {
+        time: 'Heltid',
+        days: '4-5 dagar i veckan',
+        price: '2990kr i månaden',
+      },
+      partTime: {
+        time: 'Deltid',
+        days: '2-3 dagar i veckan',
+        price: '2290kr i månaden',
+      },
+      flex: {
+        time: 'Flex',
+        price: '350kr för en dag',
+      },
     },
 
-    startPage: {
-        title: "Välkommen till Hundkojans hunddagis"
+    ourStaff: {
+      title: 'Vår personal',
+    },
+  },
+
+  blog: {
+    postsArticle: {
+      title: 'Blogg',
     },
 
-    contact: {
-        title: "Kontakta oss",
-
-        contactDetails: {
-            title: "Kontaktuppgifter",
-            name: "Namn:",
-            address: "Adress:",
-            phone: "Telefon:",
-            Email: "E-post:"
-        },
+    Pictures: {
+      title: 'Senaste bilderna',
     },
-
-    questions: {
-        title: "Vanliga Frågor",
-        subHeading: "Självklart har du som ägare av en fyrbent vän många frågor. Här har vi samlat de vanligaste frågorna med svar. Skulle svaret på din fråga inte finnas är du välkommen att kontakta oss här!",
-
-        firstQuestion: {
-            title: "Hur många hundar tar ni emot samtidigt?",
-            answar: "",
-        },
-        secondQuestion: {
-            title: "Hur ser en dag ut hos er?",
-            answar: ""
-        },
-        thirdQuestion: {
-            title: "Hur lång tid tar det att få en dagisplats?",
-            answar: ""
-        },
-        fourthQuestion: {
-            title: "Tar ni emot alla raser?",
-            answar: ""
-        },
-        fifthQuestion: {
-            title: "Kan man ha hundar över natten hos er?",
-            answar: ""
-        },
-        sixthQuestion: {
-            title: "Tar ni emot okastrerade hundar?",
-            answar: ""
-        }
-    },
-
-    about: {
-        title: "- Vi tar hand om din bästa vän",
-
-        firstParagraph: "Hundkojan hittar du på en stor familjegård bara 5 minuter från Helsingborgs centrum. Gården har stora inhängnade utomhusytor där din fyrbenta vän kan springa fritt med andra fyrbenta kompisar. Inomhusdelen renoverades under 2020 och där finns både sociala och indivduella utrymmen, allt för att din hund ska känna sig trygg.",
-
-        secondParagraph: "Hundkojan ser till att din vovve både får lugn och ro, aktivering, stimulans och härligt umgänge med andra hundar – så att du kan göra det du behöver göra!",
-
-        thirdParagraph: "Du väljer själv vilken tjänst som passar dig och din hund bäst!",
-
-        fourthParagraph: "Smulan och Bosse Hundsson grundade 1998 Hundkojan AB och har sedan dess samlat på sig väldigt många fyrbenta vänner runtom i Skåne. Verksamheten är godkänd av Länsstyrelsen.",
-
-        ourServices: {
-            title: "Våra tjänster",
-
-            fullTime: {
-                time: "Heltid",
-                days: "4-5 dagar i veckan",
-                price: "2990kr i månaden",
-            },
-            partTime: {
-                time: "Deltid",
-                days: "2-3 dagar i veckan",
-                price: "2290kr i månaden"
-            },
-            flex: {
-                time: "Flex",
-                price: "350kr för en dag"
-            }
-        },
-
-        ourStaff: {
-            title: "Vår personal",
-        }
-    },
-
-    blog: {
-        postsArticle: {
-            title: "Blogg"
-        },
-
-        Pictures: {
-            title: "Senaste bilderna"
-        },
-
-    },
-
-}
+  },
+};
