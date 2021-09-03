f6e3f63a88dd370c3be54c6778b17857e507ce25
Emma Olsson <emma.olsson22@learnet.se>
2021-09-03 08:36:47 +0200

    added blogScreen

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from '../css/blog.module.css'
 
+import blogPosts from '../assets/data/blogPosts'
+
 const BlogScreen = () => {
+
+    const [blogData, setBlogData] = useState([])
+
+    useEffect(() => {
+  
+          setBlogData(blogPosts)
+  
+    }, [])
+
+    console.log('blogdata', blogData)
+
     return <section>
         <main className={Styles.mainContainer}>
             <div className={Styles.topAbout}>
                 <article className={Styles.blogPosts}>
                     <h2>Blogg</h2>
-                        <p>
-                            Snart hittar du de senaste inläggen och artiklarna här!
-                        </p>
+                    <p>Snart hittar du de senaste inläggen och artiklarna här!</p>
+                    {
+                        blogData.map((blog) => {
+                            return <section>
+                                <p>{blog.author}</p>
+                            </section>
+                        })
+                    }
                 </article>
                 <aside className={Styles.latestPictures}>
                     <h2 className={Styles.pictureTitle}>Senaste bilderna</h2>
