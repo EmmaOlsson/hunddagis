cbfecdc7ea1b2a9ef4aa338211f84bfa5cd7a4c1
elinkronqvist <elin.kronqvist@utb.ecutbildning.se>
2021-09-06 08:50:32 +0200

    offer+price data

---
+++ src/assets/data/services.js
@@ -0,0 +1,39 @@
+
+// Swedish
+export const servicesSwe =
+{
+    title: 'Våra tjänster',
+    fullTime: {
+        time: 'Heltid',
+        days: '4-5 dagar i veckan',
+        price: '2990kr i månaden',
+    },
+    partTime: {
+        time: 'Deltid',
+        days: '2-3 dagar i veckan',
+        price: '2290kr i månaden',
+    },
+    flex: {
+        time: 'Flex',
+        price: '350kr för en dag',
+    }
+}
+// English
+export const servicesEng =
+{
+    title: 'Our services',
+    fullTime: {
+        time: 'Full time',
+        days: '4-5 days a week',
+        price: '2,990 SEK a month',
+    },
+    partTime: {
+        time: 'Part time',
+        days: '2-3 days a week',
+        price: '2,290 SEK a month',
+    },
+    flex: {
+        time: 'Flexible time',
+        price: '350 SEK for a day',
+    },
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Styles from '../css/homescreen.module.css';
 import { useHistory } from 'react-router-dom';
+import Styles from '../css/homescreen.module.css';
 import happyDogsImg from '../../src/assets/graphics/happy-dogs.jpg';
+import servicesSweFile from '../assets/data/services';
+
+import { useTranslation } from 'react-i18next';
+
 
 const HomeScreen = () => {
 
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   const history = useHistory();
+
+  const [servicesSwe, setServicesSwe] = useState([])
+
+  useEffect(() => {
+    setServicesSwe(servicesSweFile)
+  }, [])
 
   function handleClick() {
     history.push("/contact");
@@ -18,7 +34,9 @@ const HomeScreen = () => {
         <div className={Styles.topSection} >
           <img className={Styles.mainImg} src={happyDogsImg} alt="happy-dogs" ></img>
         </div>
-        <div className={Styles.bottomSection} >
+
+        <div className={Styles.middleSection} >
+
           <article className={Styles.infoBox} >
             <h3>Hundkojans hunddagis</h3>
             <p>Våra öppettider</p>
@@ -27,45 +45,28 @@ const HomeScreen = () => {
           </article>
 
           <article className={Styles.applicationBox} >
-
             <div className={Styles.applicationBoxContent} >
               <h5>Intresserad av en dagisplats?</h5>
             <button className={Styles.applicationButton} onClick={handleClick} >Skicka ansökan</button>
               </div>
-        </article>
+          </article>
+
+        </div>
+
+        <div className={Styles.bottomSection} >
+            <article className={Styles.servicesContainer} >
+              {
+                servicesSwe.map((service) => {
+                  return <section className={Styles.priceInfo} key={service.id} >
+                    <h4>{service.time}</h4>
+                    <p>{service.days}</p>
+                    <p>{service.price}</p>
+                  </section>
+                })
+              }
+            </article>
         </div>
     </section>
     );
-      
-
-  {/*       <main className={Styles.mainContainer} >
-
-      <img className={Styles.mainImg} src={happyDogsImg} alt="happy-dogs" ></img>
-
-
-      </main> */}
-{/*       <div className={Styles.mainText} >
-        <article className={Styles.infoBox} >
-          <h3>Hundkojans hunddagis</h3>
-          <p>Våra öppettider</p>
-          <p>Inlämning: 07:00 - 09:00</p>
-          <p>Hämtning: 14:00 - 17:30</p>
-        </article>
-
-
-        <article className={Styles.applicationBox} >
-          <div className={Styles.applicationBoxCircle} ></div>
-
-          <div className={Styles.applicationBoxContent} >
-            <h5>Intresserad av en dagisplats?</h5>
-          <button className={Styles.applicationButton} onClick={handleClick} >Skicka ansökan</button>
-            </div>
-
-        </article>
-
-        </div> */}
-
-
-
-      }
+  };
 export default HomeScreen;
