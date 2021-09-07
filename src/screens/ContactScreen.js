b9e38f771d24a3623600eaf9af5876ad56eb4f4e
Emma Olsson <emma.olsson22@learnet.se>
2021-09-07 16:34:56 +0200

    Added handleClick to submit-button

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -2,54 +2,99 @@ import React from 'react';
 import hunddagis from '../assets/graphics/hunddagis.jpg';
 import style from '../css/contact.module.css';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
 
 const ContactScreen = () => {
 
   const { t, i18n } = useTranslation();
 
+  const history = useHistory();
+
+  function handleClick() {
+    alert('Tack för ditt meddelande!')
+    history.push('/contact')
+  }
+
   return (
     <section className={style.logoContainer}>
       <div className={style.contactTitle}>
-        <span className={style.contactTitle}>{t('contact.title')}</span>
+        <h2 className={style.contactTitle}>{t('contact.title')}</h2>
       </div>
 
       <div className={style.contactDetailsContainer}>
-        <div>
+        <div className={style.leftContainer}>
           <img className={style.img} alt="location" src={hunddagis}></img>
-        </div>
+          <div className={style.contactDetails}>
+            <div className={style.content}>
+              <div className={style.titleContainer}>
+                <span className={style.contactDetailsTitle}>
+                  {t('contact.contactDetails.title')}
+                </span>
+              </div>
 
-        {/* // Här kan du lägga in formuläret (ta bort min lilla div om det behövs) */}
-        <div>Formulär</div>
+              <div className={style.nameContainer}>
+                <span className={style.subHeading}>{t('contact.contactDetails.name')} </span>
+                <span className={style.information}>Hunddagis</span>
+              </div>
 
-        <div className={style.contactDetails}>
-          <div className={style.content}>
-            <div className={style.titleContainer}>
-              <span className={style.contactDetailsTitle}>
-                {t('contact.contactDetails.title')}
-              </span>
-            </div>
+              <div className={style.addressContainer}>
+                <span className={style.subHeading}>{t('contact.contactDetails.address')} </span>
+                <span className={style.information}>Båthusgatan 9, 25667 Helsingborg</span>
+              </div>
 
-            <div className={style.nameContainer}>
-              <span className={style.subHeading}>{t('contact.contactDetails.name')} </span>
-              <span className={style.information}>Hunddagis</span>
-            </div>
+              <div className={style.phoneContainer}>
+                <span className={style.subHeading}>{t('contact.contactDetails.phone')} </span>
+                <span className={style.information}>000000000</span>
+              </div>
 
-            <div className={style.addressContainer}>
-              <span className={style.subHeading}>{t('contact.contactDetails.address')} </span>
-              <span className={style.information}>Båthusgatan 9, 25667 Helsingborg</span>
-            </div>
-
-            <div className={style.phoneContainer}>
-              <span className={style.subHeading}>{t('contact.contactDetails.phone')} </span>
-              <span className={style.information}>000000000</span>
-            </div>
-
-            <div className={style.emailContainer}>
-              <span className={style.subHeading}>{t('contact.contactDetails.Email')} </span>
-              <span className={style.information}>test@testsson.com</span>
+              <div className={style.emailContainer}>
+                <span className={style.subHeading}>{t('contact.contactDetails.Email')} </span>
+                <span className={style.information}>test@testsson.com</span>
+              </div>
             </div>
           </div>
+
         </div>
+
+        {/* Contact Form */}
+        <aside className={style.contactContainer}>
+          <form className={style.contactForm}>
+          <h3 className={style.contactFormTitle}>Kontaktformulär</h3>
+            <ul className={style.contactFormContact}>
+              <li>
+                <label for="name">Förnamn:</label>
+                <input type="text"></input>
+              </li>
+              <li>
+                <label for="name">Efternamn:</label>
+                <input type="text"></input>
+              </li>
+              <li>
+                <label for="name">Email:</label>
+                <input type="text"></input>
+              </li>
+              <li>
+                <label for="dropDownMenu">Ämne:</label>
+                <select className={style.dropDownMenu}>
+                  <option value="booking">Boka plats</option>
+                  <option value="questions">Fråga om tjänst</option>
+                  <option value="feedback">Feedback</option>
+                  <option value="complaint">Klagomål</option>
+                </select>
+              </li>
+              <li>
+                <label for="name">Meddelande:</label>
+                <input type="text" className={style.contactText}></input>
+              </li>
+              <li>
+                <input type="submit" value="Skicka" onClick={handleClick} className={style.submitForm}></input>
+              </li>
+            </ul>
+          </form>
+        </aside>
+
+
+
       </div>
     </section>
   );
