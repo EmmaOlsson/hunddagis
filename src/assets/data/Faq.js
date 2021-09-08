9c8e0bccd57e43f65af5ec31fd3c9652071db75d
Nina Jivetorp <ninajivetorp@gmail.com>
2021-09-08 13:42:17 +0200

    removed redundant brackets

--- src/assets/data/Faq.js
+++ src/assets/data/Faq.js
@@ -2,7 +2,7 @@ export const questionsSwe = [
   {
     title: 'Hur många hundar tar ni emot samtidigt?',
     answer:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
   },
   {
     title: 'Hur ser en dag ut hos er?',
@@ -32,36 +32,34 @@ export const questionsSwe = [
 ];
 
 export const questionsEng = [
-  [
-    {
-      title: 'How many dogs do you receive at the same time?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-    {
-      title: 'What does a day look like with you?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-    {
-      title: 'How long does it take to get a kindergarten place?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-    {
-      title: 'Do you accept all races?',
-      anwser:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-    {
-      title: 'Can you have dogs with you overnight?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-    {
-      title: 'Do you accept unneutered dogs?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
-    },
-  ],
+  {
+    title: 'How many dogs do you receive at the same time?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
+  {
+    title: 'What does a day look like with you?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
+  {
+    title: 'How long does it take to get a kindergarten place?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
+  {
+    title: 'Do you accept all races?',
+    anwser:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
+  {
+    title: 'Can you have dogs with you overnight?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
+  {
+    title: 'Do you accept unneutered dogs?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores quisquam dolorem architecto deleniti repellendus exercitationem est, ab consequuntur dolor id, veniam sed adipisci ut explicabo debitis doloribus voluptatibus ducimus Et, obcaecati nam quisquam reiciendis sit quis ab possimus recusandae eos? Sapiente eius itaque quos! Qui voluptas consectetur velit est nulla maiores cum placeat, atque fuga. A natus expedita officia?',
+  },
 ];
